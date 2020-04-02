8e0f08efb61cbc4b08e056bb4f85e306c1979a64
YamafukuKei <yamafuku.github@gmail.com>
2020-04-02 11:52:03 +0900

    Add pre process file

---
+++ .pre_process.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "***************************************"
+echo "*****      for PhoXi Control      *****"
+echo "***************************************"
+wget https://photoneo.com/files/installer/PhoXi/1.2.14/PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
+tar xf PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
+./PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.run
