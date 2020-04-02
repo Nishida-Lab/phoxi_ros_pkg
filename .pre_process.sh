aa3f34992df68d1d5da57b27c5492d325be078d5
YamafukuKei <yamafuku.github@gmail.com>
2020-04-02 13:00:04 +0900

    Modify for ci

--- .pre_process.sh
+++ .pre_process.sh
@@ -4,5 +4,6 @@ echo "*****      for PhoXi Control      *****"
 echo "***************************************"
 apt install wget
 wget https://photoneo.com/files/installer/PhoXi/1.2.14/PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
+mount -o remount,rw /
 tar xf PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
 ./PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.run
