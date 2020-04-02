708856ae062943da681a26a0981e4b7cf4acaa57
YamafukuKei <yamafuku.github@gmail.com>
2020-04-02 12:49:12 +0900

    Modify to add apt insatll wget

--- .pre_process.sh
+++ .pre_process.sh
@@ -2,6 +2,7 @@
 echo "***************************************"
 echo "*****      for PhoXi Control      *****"
 echo "***************************************"
+apt install wget
 wget https://photoneo.com/files/installer/PhoXi/1.2.14/PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
 tar xf PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
 ./PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.run
