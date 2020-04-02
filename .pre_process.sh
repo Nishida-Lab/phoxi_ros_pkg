46c7cb0ecadcf851efce382e31f13d3e9d5a9a13
YamafukuKei <yamafuku.github@gmail.com>
2020-04-02 16:18:40 +0900

    Delete

--- .pre_process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "***************************************"
-echo "*****      for PhoXi Control      *****"
-echo "***************************************"
-apt install wget
-wget https://photoneo.com/files/installer/PhoXi/1.2.14/PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
-mount -o remount,rw /
-tar xf PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.tar
-./PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE.run
